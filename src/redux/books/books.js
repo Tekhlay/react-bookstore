38c26175934c151d5c0c0d73dd71da9c437352a9
Teklay <teklay@gmail.com>
2023-01-24 14:18:34 +0300

    Add content to the initial state

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,9 +1,27 @@
+import { v4 as uuidv4 } from 'uuid';
+
 // Actions
 const Addbook = 'bookstore/books/ADD_BOOK';
 const Removebook = 'bookstore/books/REMOVE_BOOK';
 
 // intial State
-const initialState = [];
+const initialState = [
+  {
+    id: uuidv4(),
+    title: 'The sun',
+    author: 'Shangay',
+  },
+  {
+    id: uuidv4(),
+    title: 'The flood and water',
+    author: 'Ergenclop',
+  },
+  {
+    id: uuidv4(),
+    title: 'The dive and conqure',
+    author: 'Hafiz',
+  },
+];
 
 // Reducer
 const booksReducer = (state = initialState, action) => {
