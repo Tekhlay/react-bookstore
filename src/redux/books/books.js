24fe832ebb374be97ead34d4a84ca205ea6a038d
Teklay <teklay@gmail.com>
2023-01-24 11:03:47 +0300

    Setup redux toolkit

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,29 +1,9 @@
-// import { v4 as uuidv4 } from 'uuid';
 // Actions
 const Addbook = 'ADD_BOOK';
 const Removebook = 'REMOVE_BOOK';
 
 // intial State
-const initialState = [
-//   {
-//     id: uuidv4(),
-//     title: 'The sun',
-//     category: 'Action',
-//     author: 'Shang',
-//   },
-//   {
-//     id: uuidv4(),
-//     title: 'The flood and water',
-//     category: 'Action',
-//     author: 'Ergenclop',
-//   },
-//   {
-//     id: uuidv4(),
-//     title: 'The dive and conqure',
-//     category: 'Action',
-//     author: 'Hafiz',
-//   },
-];
+const initialState = [];
 
 // Reducer
 const booksReducer = (state = initialState, action) => {
