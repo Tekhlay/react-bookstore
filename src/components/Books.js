406fd7926f35499fb57d9b9bfcca50f7abbc60ec
Teklay <teklay@gmail.com>
2023-01-24 19:56:07 +0300

    Add a functionality for "Remove" button click to delete a book from the store

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,16 +1,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable import/no-named-as-default */
 import React from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import { useSelector } from 'react-redux';
 import AddnewBook from './AddnewBook';
 import Book from './Book';
 
 const Books = () => {
-  const book = [
-    { id: uuidv4(), title: 'The sun', author: 'Shangay' },
-    { id: uuidv4(), title: 'The flood and water', author: 'Ergenclop' },
-    { id: uuidv4(), title: 'The dive and conqure', author: 'Hafiz' },
-  ];
+  const book = useSelector((state) => state.books);
 
   return (
     <div className="book-container">
