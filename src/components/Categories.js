a572ebb92dc1512b0ae4eda8ad0db64f5c134df9
Teklay <teklay@gmail.com>
2023-01-25 13:40:09 +0300

    Refactor componets features to persist the changes

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { checkStatusAction } from '../redux/categories/categories';
 
 const Categories = () => {
+  const categories = useSelector((state) => state.categories);
   const dispatch = useDispatch();
   return (
-    <div className="categories">
+    <div>
       <h1>Categories</h1>
       <button type="button" onClick={() => dispatch(checkStatusAction())}>
         Check Status
       </button>
+      <p>{categories}</p>
     </div>
   );
 };
